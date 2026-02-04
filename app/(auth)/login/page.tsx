d89c9dd52fab4a40dcb61ffba11a1ab6b1617231
TrucTT <truc5302@gmail.com>
2026-02-04 11:51:48 +0700

    page login register

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function LoginPage() {
+    return (
+        <div>
+            <h1 className="text-center font-bold text-2xl"> Login Page</h1>
+        </div>
+    )
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,8 @@
+
+export default function RegisterPage() {
+    return (
+        <div>
+            <h1 className="text-center font-bold text-2xl"> Register Page</h1>
+        </div>
+    )
+}
